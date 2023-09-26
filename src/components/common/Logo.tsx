53f0ee4d9e93ebcc9352245e39f06c5d69d7ca5e
gunhwalee <ghlee2588@gmail.com>
2023-09-26 17:54:25 +0900

    fix: 이미지 경고문구 수정

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -4,7 +4,7 @@ import Location from "@/../public/logo.svg";
 export default function Logo({ size }: { size: number }) {
   return (
     <div className="h-10 flex items-center">
-      <Image src={Location} alt="logo image" width={size} height={size} />
+      <Image src={Location} alt="logo image" width={size} />
       <h2 className="pl-2 text-2xl">Algolendar</h2>
     </div>
   );
