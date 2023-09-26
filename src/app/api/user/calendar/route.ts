091ff4ebe4f4836de9235650cd4b50677352ef4d
gunhwalee <ghlee2588@gmail.com>
2023-09-26 16:58:42 +0900

    feat: 기본캘린더 설정 기능

---
+++ src/app/api/user/calendar/route.ts
@@ -0,0 +1,29 @@
+import { authConfig } from "@/lib/auth";
+import { PrismaClient } from "@prisma/client";
+import { getServerSession } from "next-auth";
+import { NextResponse } from "next/server";
+
+const prisma = new PrismaClient();
+
+export async function PATCH(req: Request) {
+  const calendarId = await req.json();
+  const session = await getServerSession(authConfig);
+  const { id } = session!;
+
+  try {
+    await prisma.user.update({
+      where: { id },
+      data: { calendar: calendarId },
+    });
+    return NextResponse.json({
+      result: "ok",
+      message: "Calendar 설정이 완료됐습니다.",
+    });
+  } catch (error) {
+    console.error("Error is: ", error);
+    return NextResponse.json({
+      result: "ng",
+      message: "잠시 후 다시 시도해주세요.",
+    });
+  }
+}
---
+++ src/components/SubmitForm.tsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useState } from "react";
+
+interface SubmitFormProps {
+  name: string;
+  value: string;
+}
+
+export default function SubmitForm({ list }: { list: SubmitFormProps[] }) {
+  const [calendar, setCalendar] = useState<string>("");
+
+  const submitOption = async () => {
+    console.log(calendar);
+    try {
+      const response = await fetch("http://localhost:3000/api/user/calendar", {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(calendar),
+      });
+      const data = await response.json();
+
+      alert(data.message);
+    } catch (error) {
+      let message;
+      if (error instanceof Error) message = error.message;
+      console.log(message);
+    }
+  };
+
+  return (
+    <div className="h-fit p-3 overflow-y-auto text-sm bg-gray-700 text-gray-200 rounded-lg">
+      {list.map((element) => (
+        <div key={element.value} className="p-2 rounded hover:bg-gray-600">
+          <div className="flex items-center">
+            <input
+              className="w-4 h-4 text-blue-600 focus:ring-blue-600 ring-offset-gray-700 focus:ring-offset-gray-700 focus:ring-2 bg-gray-600 border-gray-500"
+              type="checkbox"
+              id={element.name}
+              value={element.value}
+              checked={element.value === calendar}
+              onChange={(event) => setCalendar(event.target.value)}
+            />
+            <label
+              className="w-full ml-2 text-sm font-medium rounded text-gray-300"
+              htmlFor={element.name}
+            >
+              {element.name}
+            </label>
+          </div>
+        </div>
+      ))}
+      <button
+        className="w-full bg-gray-700 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded"
+        onClick={submitOption}
+      >
+        설정
+      </button>
+    </div>
+  );
+}
